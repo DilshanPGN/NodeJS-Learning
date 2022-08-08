abc51f4320d363cb3f92ec35dedbb634032519b9
DilshanPGN <73216265+DilshanPGN@users.noreply.github.com>
2022-08-08 17:02:12 +0530

    class

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const person = require('./person');
+const Person = require('./person');
 
-console.log(person);
+const person1 = new Person('nipun' , 24);
+
+person1.greeting();
+
+console.log(__filename);
